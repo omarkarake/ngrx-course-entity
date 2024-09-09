6861ceb71fa147e0a796471233b61c31f452ba6d
omar karake <omar.karake@amalitechtraining.org>
2024-09-09 18:21:47 +0200

    Implementing Reducers Using the NgRx Entity Adapter

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,48 +1,52 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HomeComponent} from './home/home.component';
-import {CoursesCardListComponent} from './courses-card-list/courses-card-list.component';
-import {EditCourseDialogComponent} from './edit-course-dialog/edit-course-dialog.component';
-import {CoursesHttpService} from './services/courses-http.service';
-import {CourseComponent} from './course/course.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {RouterModule, Routes} from '@angular/router';
-import { EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
-import {compareCourses, Course} from './model/course';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./home/home.component";
+import { CoursesCardListComponent } from "./courses-card-list/courses-card-list.component";
+import { EditCourseDialogComponent } from "./edit-course-dialog/edit-course-dialog.component";
+import { CoursesHttpService } from "./services/courses-http.service";
+import { CourseComponent } from "./course/course.component";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterModule, Routes } from "@angular/router";
+import {
+  EntityDataService,
+  EntityDefinitionService,
+  EntityMetadataMap,
+} from "@ngrx/data";
+import { compareCourses, Course } from "./model/course";
 
-import {compareLessons, Lesson} from './model/lesson';
-import { CourseResolver } from './courses.resolver';
-import { EffectsModule } from '@ngrx/effects';
-import { CourcesEffects } from './cources.effects';
-
+import { compareLessons, Lesson } from "./model/lesson";
+import { CourseResolver } from "./courses.resolver";
+import { EffectsModule } from "@ngrx/effects";
+import { CourcesEffects } from "./cources.effects";
+import { StoreModule } from "@ngrx/store";
+import { coursesReducer } from "./reducers/course.reducers";
 
 export const coursesRoutes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomeComponent,
     resolve: {
-      courses: CourseResolver
-    }
-
+      courses: CourseResolver,
+    },
   },
   {
-    path: ':courseUrl',
-    component: CourseComponent
-  }
+    path: ":courseUrl",
+    component: CourseComponent,
+  },
 ];
 
 @NgModule({
@@ -64,31 +68,24 @@ export const coursesRoutes: Routes = [
     MatMomentDateModule,
     ReactiveFormsModule,
     RouterModule.forChild(coursesRoutes),
-    EffectsModule.forFeature([CourcesEffects])
+    EffectsModule.forFeature([CourcesEffects]),
+    StoreModule.forFeature("courses", coursesReducer),
   ],
   declarations: [
     HomeComponent,
     CoursesCardListComponent,
     EditCourseDialogComponent,
-    CourseComponent
+    CourseComponent,
   ],
   exports: [
     HomeComponent,
     CoursesCardListComponent,
     EditCourseDialogComponent,
-    CourseComponent
+    CourseComponent,
   ],
   entryComponents: [EditCourseDialogComponent],
-  providers: [
-    CoursesHttpService,
-    CourseResolver
-  ]
+  providers: [CoursesHttpService, CourseResolver],
 })
 export class CoursesModule {
-
-  constructor() {
-
-  }
-
-
+  constructor() {}
 }
