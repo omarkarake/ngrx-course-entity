a66fe064ac15485e2072fd854aed7fc54abbc9a3
omar karake <omar.karake@amalitechtraining.org>
2024-09-10 11:57:22 +0200

    NgRx Entity Adapter Configuration - Understanding sortComparer and selectId

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,11 +1,13 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
-import { Course } from "../model/course";
+import { compareCourses, Course } from "../model/course";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
 
 export interface CoursesState extends EntityState<Course> {}
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses,
+});
 export const initialCoursesState = adapter.getInitialState();
 export const coursesReducer = createReducer(
   initialCoursesState,
